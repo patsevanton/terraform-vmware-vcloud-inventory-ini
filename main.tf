f10a4c073681e65dd4e9c30836adfe3138d1ed24
Anton Patsev <patsev.anton@gmail.com>
2021-04-21 19:50:37 +0600

    change param provider vcd

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 # Настройка провайдера для подключения к vCloud Director
 provider "vcd" {
-  user                 = var.vcd_user
-  password             = var.vcd_pass
-  auth_type            = var.auth_type
-  org                  = var.vcd_org
-  vdc                  = var.vcd
-  url                  = var.vcd_url
+  user                 = var.vcd_org_user
+  password             = var.vcd_org_password
+  auth_type            = "integrated"
+  org                  = var.vcd_org_org
+  vdc                  = var.vcd_org_vdc
+  url                  = var.vcd_org_url
   max_retry_timeout    = 30
   allow_unverified_ssl = true
 }
