84c9e9ebc44a7b8519509043265acf4e35d2b706
Anton Patsev <patsev.anton@gmail.com>
2021-04-28 10:24:08 +0600

    terraform fmt

--- main.tf
+++ main.tf
@@ -21,39 +21,39 @@ provider "vcd" {
 
 # Create routed org-network
 resource "vcd_network_routed" "MyAppNet" {
-  name = "MyAppNet"
+  name         = "MyAppNet"
   edge_gateway = var.vcd_org_edge_name
-  gateway = "10.1.0.1"
+  gateway      = "10.1.0.1"
   dhcp_pool {
     start_address = "10.1.0.15"
-    end_address = "10.1.0.20"
+    end_address   = "10.1.0.20"
   }
 }
 
 # vApp Name and Metadata
 resource "vcd_vapp" "MyApp" {
-  name = "MyApp"
+  name     = "MyApp"
   power_on = "true"
 }
 
 # vApp network connected to routed org-network
 resource "vcd_vapp_org_network" "MyAppNet" {
-  vapp_name = "MyApp"
+  vapp_name        = "MyApp"
   org_network_name = vcd_network_routed.MyAppNet.name
 }
 
 resource "vcd_vapp_vm" "WebServer" {
-  vapp_name = vcd_vapp.MyApp.name
-  name = "WebServer"
+  vapp_name     = vcd_vapp.MyApp.name
+  name          = "WebServer"
   catalog_name  = var.vcd_org_catalog
   template_name = var.template_vm
-  memory = 512
-  cpus       = 2
-  cpu_cores  = 2
+  memory        = 512
+  cpus          = 2
+  cpu_cores     = 2
 
   network {
-    type = "org"
-    name = vcd_network_routed.MyAppNet.name
+    type               = "org"
+    name               = vcd_network_routed.MyAppNet.name
     ip_allocation_mode = "DHCP"
   }
   depends_on = [vcd_vapp.MyApp]
